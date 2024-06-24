717afa441ef3d7f206ac32b26db56795883d483e
jimh <james.l.heinecke@gmail.com>
2024-06-24 13:22:51 -0500

    updates

--- shipping_app/api_test_label.js
+++ shipping_app/api_test_label.js
@@ -3,108 +3,112 @@ const API_KEY = '335b49e04a28f9d5d675e7c7d0e5d8907ca5d9c218506c1e04aa14be8a6799f
 (async () => {
     try {
         const requestBody = {
-            "currencyCode": "usd",
-"shipmentParameters": {
-"partnerShipmentId": null,
-"orderedDateTime": "2024-06-05T22:04:58.203Z",
-"shippedDateTime": "2024-06-05T22:04:58.203Z",
-"desiredDeliveryDate": null,
-"carrierName": null,
-"carrierTrackingId": null,
-"orderItemQuantities": [{
-"productId": "123456",
-"quantity": 1,
-"productDetails": [],
-"shipiumOrderId": null,
-"hazmat": false,
-"hazmatInfo": {
-"category": null,
-"quantity": 0,
-"quantityType": null,
-"quantityUnits": null,
-"containerType": null,
-"hazmatId": null,
-"properShippingName": null,
-"packingGroup": null,
-"transportMode": null,
-"packingInstructionCode": null,
-"hazardClass": null,
-"subsidiaryClasses": null
-}
-}],
-"shipFromAddress": {
-"name": "20-NJ",
-"phoneNumber": null,
-"phoneNumberCountryCode": null,
-"emailAddress": null,
-"company": "N/A",
-"street1": "14E Easy Street Bound Brook, NJ 08805",
-"street2": null,
-"city": "Bound Brook",
-"state": "NJ",
-"countryCode": "US",
-"postalCode": "08805",
-"addressType": "commercial"
-},
-"destinationAddress": {
-"name": "Chelsea Jijawi",
-"phoneNumber": "5555555555",
-"phoneNumberCountryCode": null,
-"emailAddress": null,
-"company": "N/A",
-"street1": "4207 N Elsinore Ave",
-"street2": null,
-"city": "Meridian",
-"state": "ID",
-"countryCode": "US",
-"postalCode": "83646",
-"addressType": "residential"
-},
-"shipOption": null,
-"packagingType": {
-"packagingMaterial": "box",
-"packagingSizeName": "test-package",
-"packagingTypeId": null,
-"linearDimensions": {
-"linearUnit": "in",
-"length": 9,
-"width": 7,
-"height": 3
-},
-"packagingWeight": {
-"weightUnit": "lb",
-"weight": 3
-}
-},
-"totalWeight": {
-"weightUnit": "lb",
-"weight": 3
-},
-"shipmentTags": [],
-"customsInfo": null,
-"saturdayDelivery": false,
-"fulfillmentContext": "test-tenant",
-"fulfillmentType": "customer",
-"tenantId": "test-tenant"
-},
-"generateLabel": true,
-"includeEvaluatedServiceMethodsInResponse": false,
-"labelParameters": {
-"currencyCode": "usd",
-"labelFormats": ["png", "zpl"],
-"includeLabelImagesInResponse": true,
-"customLabelEntries": {},
-"testMode": true
-},
-"asOfDate": null,
-"carrierServiceMethodAllowList": []
+            "currencyCode": "usd", //required
+            "shipmentParameters": {
+                "partnerShipmentId": null,
+                "orderedDateTime": "2024-06-05T22:04:58.203Z",
+                "shippedDateTime": "2024-06-05T22:04:58.203Z",
+                "desiredDeliveryDate": null,
+                "carrierName": null,
+                "carrierTrackingId": null,
+                "orderItemQuantities": [{
+                    "productId": "554433",//required
+                    "quantity": 2,//required
+                    "productDetails": ["details"],//required
+                    "shipiumOrderId": null,
+                    "hazmat": false,
+                    "hazmatInfo": {
+                        "category": null,
+                        "quantity": 0,
+                        "quantityType": null,
+                        "quantityUnits": null,
+                        "containerType": null,
+                        "hazmatId": null,
+                        "properShippingName": null,
+                        "packingGroup": null,
+                        "transportMode": null,
+                        "packingInstructionCode": null,
+                        "hazardClass": null,
+                        "subsidiaryClasses": null
+                    }
+                }],
+                "shipFromAddress": { //required
+                    "name": "20-NJ",
+                    "phoneNumber": null,
+                    "phoneNumberCountryCode": null,
+                    "emailAddress": null,
+                    "company": "N/A",
+                    "street1": "14E Easy Street Bound Brook, NJ 08805",
+                    "street2": null,
+                    "city": "Bound Brook",
+                    "state": "NJ",
+                    "countryCode": "US",
+                    "postalCode": "08805",
+                    "addressType": "commercial"
+                },
+                "destinationAddress": { //required
+                    "name": "Chelsea Jijawi",
+                    "phoneNumber": "5555555555",
+                    "phoneNumberCountryCode": null,
+                    "emailAddress": null,
+                    "company": "N/A",
+                    "street1": "4207 N Elsinore Ave",
+                    "street2": null,
+                    "city": "Meridian",
+                    "state": "ID",
+                    "countryCode": "US",
+                    "postalCode": "83646",
+                    "addressType": "residential"
+                },
+                "shipOption": null,
+                "packagingType": {
+                    "packagingMaterial": "box",
+                    "packagingSizeName": "test-package",
+                    "packagingTypeId": null,
+                    "linearDimensions": {
+                        "linearUnit": "in",
+                        "length": 9,
+                        "width": 7,
+                        "height": 3
+                    },
+                    "packagingWeight": {
+                        "weightUnit": "lb",
+                        "weight": 3
+                    }
+                },
+                "totalWeight": {
+                    "weightUnit": "lb",
+                    "weight": 3
+                },
+                "shipmentTags": [],
+                "customsInfo": null,
+                "saturdayDelivery": false,
+                "fulfillmentContext": "test-tenant",
+                "fulfillmentType": "customer",
+                "tenantId": "test-tenant"
+            },
+            "generateLabel": true,
+            "includeEvaluatedServiceMethodsInResponse": false,
+            "labelParameters": {
+                "currencyCode": "usd",
+                "labelFormats": ["png", "zpl"],
+                "includeLabelImagesInResponse": true,
+                "customLabelEntries": {},
+                "testMode": true,
+                "contentDescription": "test-description",
+            },
+            "asOfDate": null,
+            "carrierServiceMethodAllowList": []
         };
+
+        console.log("Request Body:", JSON.stringify(requestBody, null, 2));
 
         const response = await fetch('https://api.shipium.com/api/v1/shipment/carrierselection/label', {
             method: 'POST',
             headers: {
                 'Authorization': `Basic ${Buffer.from(API_KEY + ':').toString('base64')}`,
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'accept': 'application/json',
             },
             body: JSON.stringify(requestBody)
         });
--- shipping_app/api_test_label.py
+++ shipping_app/api_test_label.py
@@ -1,91 +1,119 @@
-from flask import Flask, request, jsonify
 import requests
 import base64
 import json
 
-app = Flask(__name__)
-
 API_KEY = '335b49e04a28f9d5d675e7c7d0e5d8907ca5d9c218506c1e04aa14be8a6799f2'  # Replace with your actual API key
 
-# Mock function to validate address
-def validate_address(address):
-    if address["countryCode"] == "US" and address["postalCode"].isdigit():
-        return {"valid": True}
-    else:
-        return {"valid": False}
-
-@app.route('/validate_address', methods=['POST'])
-def validate_address_endpoint():
-    address = request.json.get('address')
-    if not address:
-        return jsonify({"error": "address is required"}), 400
-    
-    validation_result = validate_address(address)
-    return jsonify(validation_result)
-
-# Mock function to get order info
-def get_order_info(order_id):
-    return {
-        "order_id": order_id,
-        "customer_name": "John Doe",
-        "address": "123 Main St, Anytown, USA"
+try:
+    request_body = {
+        "currencyCode": "usd",
+        "shipmentParameters": {
+            "partnerShipmentId": None,
+            "orderedDateTime": "2024-06-05T22:04:58.203Z",
+            "shippedDateTime": "2024-06-05T22:04:58.203Z",
+            "desiredDeliveryDate": None,
+            "carrierName": None,
+            "carrierTrackingId": None,
+            "orderItemQuantities": [{
+                "productId": "123456",
+                "quantity": 1,
+                "productDetails": [],
+                "shipiumOrderId": None,
+                "hazmat": False,
+                "hazmatInfo": {
+                    "category": None,
+                    "quantity": 0,
+                    "quantityType": None,
+                    "quantityUnits": None,
+                    "containerType": None,
+                    "hazmatId": None,
+                    "properShippingName": None,
+                    "packingGroup": None,
+                    "transportMode": None,
+                    "packingInstructionCode": None,
+                    "hazardClass": None,
+                    "subsidiaryClasses": None
+                }
+            }],
+            "shipFromAddress": {
+                "name": "20-NJ",
+                "phoneNumber": None,
+                "phoneNumberCountryCode": None,
+                "emailAddress": None,
+                "company": "N/A",
+                "street1": "14E Easy Street Bound Brook, NJ 08805",
+                "street2": None,
+                "city": "Bound Brook",
+                "state": "NJ",
+                "countryCode": "US",
+                "postalCode": "08805",
+                "addressType": "commercial"
+            },
+            "destinationAddress": {
+                "name": "Chelsea Jijawi",
+                "phoneNumber": "5555555555",
+                "phoneNumberCountryCode": None,
+                "emailAddress": None,
+                "company": "N/A",
+                "street1": "4207 N Elsinore Ave",
+                "street2": None,
+                "city": "Meridian",
+                "state": "ID",
+                "countryCode": "US",
+                "postalCode": "83646",
+                "addressType": "residential"
+            },
+            "shipOption": None,
+            "packagingType": {
+                "packagingMaterial": "box",
+                "packagingSizeName": "test-package",
+                "packagingTypeId": None,
+                "linearDimensions": {
+                    "linearUnit": "in",
+                    "length": 9,
+                    "width": 7,
+                    "height": 3
+                },
+                "packagingWeight": {
+                    "weightUnit": "lb",
+                    "weight": 3
+                }
+            },
+            "totalWeight": {
+                "weightUnit": "lb",
+                "weight": 3
+            },
+            "shipmentTags": [],
+            "customsInfo": None,
+            "saturdayDelivery": False,
+            "fulfillmentContext": "test-tenant",
+            "fulfillmentType": "customer",
+            "tenantId": "test-tenant"
+        },
+        "generateLabel": True,
+        "includeEvaluatedServiceMethodsInResponse": False,
+        "labelParameters": {
+            "currencyCode": "usd",
+            "labelFormats": ["png", "zpl"],
+            "includeLabelImagesInResponse": True,
+            "customLabelEntries": {},
+            "testMode": True
+        },
+        "asOfDate": None,
+        "carrierServiceMethodAllowList": []
     }
 
-# Mock function to generate shipping label
-def generate_shipping_label(order_info):
-    return {
-        "label_url": "http://example.com/label.pdf"
+    headers = {
+        'Authorization': f'Basic {base64.b64encode((API_KEY + ":").encode()).decode()}',
+        'Content-Type': 'application/json'
     }
 
-# Mock function to print the label
-def print_label(label_url):
-    print(f"Printing label from {label_url}")
+    response = requests.post('https://api.shipium.com/api/v1/shipment/carrierselection/label', headers=headers, data=json.dumps(request_body))
 
-@app.route('/get_order_info', methods=['GET'])
-def order_info():
-    order_id = request.args.get('order_id')
-    if not order_id:
-        return jsonify({"error": "order_id is required"}), 400
-    
-    order_info = get_order_info(order_id)
-    return jsonify(order_info)
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status code {response.status_code}: {response.text}")
 
-@app.route('/generate_label', methods=['POST'])
-def generate_label():
-    order_info = request.json
-    if not order_info:
-        return jsonify({"error": "order_info is required"}), 400
-    
-    label_info = generate_shipping_label(order_info)
-    return jsonify(label_info)
-
-@app.route('/print_label', methods=['POST'])
-def print_label_endpoint():
-    label_url = request.json.get('label_url')
-    if not label_url:
-        return jsonify({"error": "label_url is required"}), 400
-    
-    print_label(label_url)
-    return jsonify({"status": "Label printed successfully"})
-
-@app.route('/create_shipment', methods=['POST'])
-def create_shipment():
-    try:
-        request_body = request.json
-        headers = {
-            'Authorization': f'Basic {base64.b64encode((API_KEY + ":").encode()).decode()}',
-            'Content-Type': 'application/json'
-        }
-
-        response = requests.post('https://api.shipium.com/api/v1/shipment/carrierselection/label', headers=headers, data=json.dumps(request_body))
-
-        if response.status_code != 200:
-            return jsonify({"error": f"Request failed with status code {response.status_code}: {response.text}"}), response.status_code
-
-        result = response.json()
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    result = response.json()
+    print(result)
+except Exception as e:
+    print(e)
